830212f2d8d1556d363321f88a8b12f294291ec5
eva <eveloz@gmail.com>
2017-12-13 20:09:20 -0800

    indentation-comments
    Fixed indentation thru PEP8 online check.
    Added comments.

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,40 +3,49 @@ import media
 
 """ Create Movie instances """
 
-toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
-                        
-avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=-9ceBgWV8io")
+# Toy Story Movie : movie title, story line, poster image and image trailer
+toy_story = media.Movie(
+    "Toy Story",
+    "A story of a boy and his toys that come to life",
+    "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+    "https://www.youtube.com/watch?v=vwyZH85NQC4")
+# Avatar Movie : movie title, story line, poster image and image trailer
+avatar = media.Movie(
+    "Avatar",
+    "A marine on an alien planet",
+    "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+    "https://www.youtube.com/watch?v=-9ceBgWV8io")
 
-godfather = media.Movie("The Godfather",
-                        "A story about the trials of the Corleone family",
-                        "https://upload.wikimedia.org/wikipedia/en/4/47/The_Godfather.jpg",
-                        "https://www.youtube.com/watch?v=sY1S34973zA")
+# The Godfather Movie : movie title, story line, poster image and image trailer
+godfather = media.Movie(
+    "The Godfather",
+    "A story about the trials of the Corleone family",
+    "https://upload.wikimedia.org/wikipedia/en/4/47/The_Godfather.jpg",
+    "https://www.youtube.com/watch?v=sY1S34973zA")
 
+# Hunger Games : movie title, story line, poster image and image trailer
+hunger_games = media.Movie(
+    "Hunger Games",
+    "A really real reality show",
+    "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+    "https://www.youtube.com/watch?v=PbA63a7H0bo")
 
-hunger_games = media.Movie("Hunger Games",
-                        "A really real reality show",
-                        "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
-                        "https://www.youtube.com/watch?v=PbA63a7H0bo")
+# Up : movie title, story line, poster image and image trailer
+up = media.Movie(
+    "Up",
+    "A 78 year old balloon salesman, is about to fulfill a lifelong dream",
+    "https://upload.wikimedia.org/wikipedia/en/0/05/Up_%282009_film%29.jpg",
+    "https://www.youtube.com/watch?v=ORFWdXl_zJ4")
 
-up = media.Movie("Up",
-                "A 78 year old balloon salesman, is about to fulfill a lifelong dream",
-                "https://upload.wikimedia.org/wikipedia/en/0/05/Up_%282009_film%29.jpg",
-                "https://www.youtube.com/watch?v=ORFWdXl_zJ4")
+# The Shining : movie title, story line, poster image and image trailer
+shining = media.Movie(
+    "The Shining",
+    "A chilling movie about a writer who becomes a winter hotel caretaker",
+    "https://upload.wikimedia.org/wikipedia/en/2/25/The_Shining_poster.jpg",
+    "https://www.youtube.com/watch?v=3b726feAhdU")
 
-shining = media.Movie("The Shining",
-                "A chilling movie about a writer who becomes a winter hotel caretaker",
-                "https://upload.wikimedia.org/wikipedia/en/2/25/The_Shining_poster.jpg",
-                "https://www.youtube.com/watch?v=3b726feAhdU")
-
+# Set the movies that will be passed to the media file
 movies = [toy_story, avatar, godfather, hunger_games, up, shining]
 
-""" Open browser """
+# Open the HTML file in a webbrowser via the fresh_tomatoes.py
 fresh_tomatoes.open_movies_page(movies)
-
-
--- media.py
+++ media.py
@@ -1,15 +1,16 @@
 import webbrowser
 
+
 class Movie():
-    """ This class provides a way to store movie related information"""
-    
+    """This class provides a way to store movie information and show trailer"""
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+        """Constructor that sets movie title, storyline, poster and trailer"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """Method that opens browser and play the movie trailer"""
         webbrowser.open(self.trailer_youtube_url)
-        
